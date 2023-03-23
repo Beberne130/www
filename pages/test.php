d3c406a50f4faeb3df7c39f9d49e2716a02302ea
Lucas Spitzer <spitzerl@proton.me>
2023-03-23 10:50:30 +0100

    Test ajout champ bdd

--- pages/test.php
+++ pages/test.php
@@ -2,26 +2,20 @@
 $servername = "eb67u.myd.infomaniak.com";
 $username = "eb67u_site";
 $password = "MDPsparkless30";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+$dbname = "eb67u_sparkless";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-  VALUES ('John', 'Doe', 'john@example.com')";
+  $sql = "INSERT INTO users (nom, prenom, age, email, passwd, nbCigaretteInscription)
+          VALUES ('John', 'Doe', 17, 'john@example.com', )";
   // use exec() because no results are returned
   $conn->exec($sql);
-  echo "New record created successfully";
-} catch(PDOException $e) {
+  echo "Nouvel utilisateur créé avec succès :D";
+}
+
+catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
 ?>
