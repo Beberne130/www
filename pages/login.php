cab74eb3ac9d69323479ac39fe3de5d6ecdca49d
Lucas Spitzer <spitzerl@proton.me>
2023-04-01 20:08:29 +0200

    Synthétisation des fichiers register et login

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,51 @@
+<?php
+$message = "";
+if (isset($_POST['submit'])) { //check if form was submitted
+    session_start();
+    if (isset($_POST['email']) && isset($_POST['passwd'])) {
+        // connexion à la base de données
+        $db_username = 'eb67u_site';
+        $db_password = 'MDPsparkless30';
+        $db_name = 'eb67u_sparkless';
+        $db_host = 'eb67u.myd.infomaniak.com';
+        $db = mysqli_connect($db_host, $db_username, $db_password, $db_name)
+            or die('could not connect to database');
+
+        // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
+        // pour éliminer toute attaque de type injection SQL et XSS
+        $email = mysqli_real_escape_string($db, htmlspecialchars($_POST['email']));
+        $passwd = mysqli_real_escape_string($db, htmlspecialchars($_POST['passwd']));
+
+        if ($email !== "" && $passwd !== "") {
+            $requete = "SELECT count(*) FROM users where email = '" . $email . "' and passwd = '" . $passwd . "' ";
+            $exec_requete = mysqli_query($db, $requete);
+            $reponse = mysqli_fetch_array($exec_requete);
+            $count = $reponse['count(*)'];
+            if ($count == 1) // nom d'utilisateur et mot de passe correctes
+            {
+                $_SESSION['email'] = $email;
+                var_dump("test1");
+                header('Location: personnal.php');
+                exit();
+            } else {
+                var_dump("test2");
+                header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+                exit();
+            }
+        } else {
+            var_dump("test3");
+            header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
+            exit();
+        }
+    } else {
+        var_dump("test4");
+        header('Location: login.php?erreur=3');
+        exit();
+    }
+    mysqli_close($db); // fermer la connexion
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -24,7 +72,7 @@
 <body class="bg-light">
     <main class="container text-center">
         <div class="row justify-content-md-center align-items-center" style="min-height: 100vh">
-            <form id="form-signup" action="login-prog.php" method="POST">
+            <form id="form-login" action="" method="POST">
                 <a href="../index.html"><img class="mb-4" src="../img/icon/android-chrome-192x192.png" alt="" width="72" height="72" /></a>
                 <h2 class="mb-3 text-center">Connexion</h2>
 
