295bc5709e6f66a79b9323e4281f0839878422b8
Lucas Spitzer <spitzerl@proton.me>
2023-04-08 16:53:34 +0200

    Retrait du graphique inutilisé et ajout de pt-3

--- pages/personal.php
+++ pages/personal.php
@@ -17,80 +17,75 @@ if (!isset($_SESSION['email'])) {
 
 <!DOCTYPE html>
 <html lang="fr">
+	<head>
+		<!-- Liens externes -->
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Ma page - Sparkless</title>
+		<!-- SOURCES -->
+		<link rel="icon" href="/img/icon/favicon.ico" />
+		<!-- Font Awesome -->
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+		<!-- MDB -->
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet" />
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
+		<!-- Style CSS -->
+		<link rel="stylesheet" href="/src/style.css" />
+		<!-- ChartJS -->
+		<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+	</head>
 
-<head>
-    <!-- Liens externes -->
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Ma page - Sparkless</title>
-    <!-- SOURCES -->
-    <link rel="icon" href="/img/icon/favicon.ico" />
-    <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-    <!-- MDB -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet" />
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
-    <!-- Style CSS -->
-    <link rel="stylesheet" href="/src/style.css" />
-    <!-- ChartJS -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-</head>
+	<body>
+		<?php require_once("../src/navbar.php") ?>
+		<main class="pt-3">
+			<!-- Jumbotron -->
+			<div class="jumbotron jumbotron-fluid">
+				<div class="container">
+					<h1 class="display-4">
+						Bievenue
+						<?php echo $prenom; ?>
+						sur votre page personelle
+					</h1>
+					<p class="lead">Sur cette page, vous pouvez suivre votre consommation de cigarettes.</p>
+				</div>
+			</div>
 
-<body>
-    <?php require_once("../src/navbar.php") ?>
-    <!-- Jumbotron -->
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">Bievenue <?php echo $prenom; ?> sur votre page personelle</h1>
-            <p class="lead">Sur cette page, vous pouvez suivre votre consommation de cigarettes.</p>
-        </div>
-    </div>
+			<!-- Contenu de la page -->
+			<div class="container">
+				<!-- Consommation quotidienne -->
+				<div class="row">
+					<div class="col-md-6">
+						<div class="card">
+							<div class="card-body">
+								<h5 class="card-title">Consommation quotidienne</h5>
+								<p class="card-text">Vous avez fumé XX cigarettes aujourd'hui.</p>
+							</div>
+						</div>
+					</div>
+				</div>
 
-    <!-- Contenu de la page -->
-    <div class="container">
-
-        <!-- Consommation quotidienne -->
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Consommation quotidienne</h5>
-                        <p class="card-text">Vous avez fumé XX cigarettes aujourd'hui.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Consommation moyenne hebdomadaire et mensuelle -->
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Consommation moyenne hebdomadaire</h5>
-                        <p class="card-text">En moyenne, vous fumez XX cigarettes par jour cette semaine.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Consommation moyenne mensuelle</h5>
-                        <p class="card-text">En moyenne, vous fumez XX cigarettes par jour ce mois-ci.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Graphique de la consommation de la semaine -->
-        <div class="row">
-            <div class="col-md-12">
-                <canvas id="myChart"></canvas>
-            </div>
-        </div>
-
-    </div>
-</body>
-
+				<!-- Consommation moyenne hebdomadaire et mensuelle -->
+				<div class="row">
+					<div class="col-md-6">
+						<div class="card">
+							<div class="card-body">
+								<h5 class="card-title">Consommation moyenne hebdomadaire</h5>
+								<p class="card-text">En moyenne, vous fumez XX cigarettes par jour cette semaine.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="card-body">
+								<h5 class="card-title">Consommation moyenne mensuelle</h5>
+								<p class="card-text">En moyenne, vous fumez XX cigarettes par jour ce mois-ci.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	</body>
 </html>
