0d5b742e72c173e686918dbcf902cfeb5820457c
Clément Bernard <beberne130630@gmail.com>
2023-04-20 16:47:54 +0200

    .

--- pages/ressources.php
+++ pages/ressources.php
@@ -19,19 +19,30 @@
 		<link rel="stylesheet" href="/src/style.css" />
 	</head>
 	<body>
-        <?php require_once("../src/navbar.php"); ?>
+		<?php require_once("../src/navbar.php"); ?>
+
 		<div class="container pt-3">
 			<h2>Diaporama de présentation du projet</h2>
-			<div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
- padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
- border-radius: 8px; will-change: transform;">
-  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFf_SyW1xQ&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-  </iframe>
-</div>
-<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFf_SyW1xQ&#x2F;view?utm_content=DAFf_SyW1xQ&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener"></a>  
+			<div style="position: relative; width: 100%; height: 0; padding-top: 56.25%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63, 69, 81, 0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform">
+				<iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0" src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFf_SyW1xQ&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen"> </iframe>
+			</div>
+			<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFf_SyW1xQ&#x2F;view?utm_content=DAFf_SyW1xQ&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener"></a>
 		</div>
-		
+
+		<div class="container pt-3">
+			<h2>Qu'est-ce que le tabagisme ?</h2>
+			<div>
+				<div class="card">
+					<div class="card-body">
+						<p class="card-text">
+							Le tabagisme est une habitude qui tue. Selon l'Organisation mondiale de la santé, plus de 8 millions de personnes meurent chaque année des suites du tabagisme, et plus de 1,2 million de personnes meurent de maladies liées à l'exposition à la fumée secondaire. Les cigarettes contiennent plus de 7 000 produits chimiques, dont plus de 70 sont connus pour causer des cancers. Environ 80% des fumeurs commencent avant l'âge de 18 ans, et 70% des fumeurs veulent arrêter de
+							fumer. Le tabagisme est non seulement dangereux pour la santé, mais aussi coûteux, car les fumeurs dépensent souvent une grande partie de leur argent pour acheter des cigarettes. Il est important de comprendre les risques associés au tabagisme et de chercher des moyens d'arrêter de fumer pour préserver sa santé et sa qualité de vie.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<?php require_once("../src/footer.php"); ?>
 	</body>
 </html>
